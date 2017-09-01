9f9e870118c330b0ee3f79dfe94d6e6fcefab60c
raspberry <renn12@naver.com>
2017-09-01 11:42:32 +0900

    fl

---
+++ flask_3.py
@@ -0,0 +1,21 @@
+from flask import Flask # /usr/lib/python2.7/dist-packages/flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'This is the Homepage'
+
+@app.route('/test')
+def test():
+        return 'test'
+
+@app.route('/tuna')
+def tuna():
+        return '<h2>Tuna is Good</h2>'
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port=8888)
