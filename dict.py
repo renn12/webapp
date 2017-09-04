70bb467109cec5def706b8e487d43f2a5649aa6c
raspberry <renn12@naver.com>
2017-09-04 11:51:51 +0900

    tem

---
+++ dict.py
@@ -0,0 +1,22 @@
+def Articles():
+	articles = [
+		{'id': 1,
+		'title': 'Article One',
+		'body': 'Hello Dict',
+		'author': 'ilhak',
+		'create_date': '04-09-2017'
+		},
+		{'id': 2,
+		'title': 'Article One',
+		'body': 'Hello Dict',
+		'author': 'kildong',
+		'create_date': '04-09-2017'
+		},		
+		{'id': 3,
+		'title': 'Article One',
+		'body': 'Hello Dict',
+		'author': 'young',
+		'create_date': '04-09-2017'
+		}
+]
+	return articles
